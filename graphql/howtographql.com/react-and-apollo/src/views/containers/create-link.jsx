db6baa63f496c9fb2678938c392493e9e1c8d268
Arthur Denner <arthurdenner7@gmail.com>
2017-08-16 10:53:40 -0300

    Fixing wrong import

--- graphql/howtographql.com/react-and-apollo/src/views/containers/create-link.jsx
+++ graphql/howtographql.com/react-and-apollo/src/views/containers/create-link.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { graphql } from 'react-apollo';
 import { get } from 'lodash/fp';
 import { Button, Input, Form } from 'antd';
-import { LINKS_PER_PAGE } from 'constants';
+import { LINKS_PER_PAGE } from '~/constants';
 import { ALL_LINKS_QUERY, CREATE_LINK_MUTATION } from '~/gql-queries';
 import FlexElement from '~/views/components/flex-element';
 import actions from '~/store/actions';
@@ -48,17 +48,21 @@ class CreateLink extends PureComponent {
             const first = LINKS_PER_PAGE;
             const skip = 0;
             const orderBy = 'createdAt_DESC';
-            const data = store.readQuery({
-              query: ALL_LINKS_QUERY,
-              variables: { first, skip, orderBy },
-            });
-            data.allLinks.splice(0, 0, createLink);
-            data.allLinks.pop();
-            store.writeQuery({
-              query: ALL_LINKS_QUERY,
-              data,
-              variables: { first, skip, orderBy },
-            });
+            try {
+              const data = store.readQuery({
+                query: ALL_LINKS_QUERY,
+                variables: { first, skip, orderBy },
+              });
+              data.allLinks.slice(0, 0, createLink);
+              data.allLinks.pop();
+              store.writeQuery({
+                query: ALL_LINKS_QUERY,
+                data,
+                variables: { first, skip, orderBy },
+              });
+            } catch (e) {
+              // console.log(e);
+            }
           },
         });
         history.push('/new/1');

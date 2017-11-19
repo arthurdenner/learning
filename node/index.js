ab33affc336fed2fe4254089643ccbe45dd248e9
arthurdenner <arthurdenner7@gmail.com>
2017-11-19 19:42:31 -0300

    Using Redis for persistence

--- node/index.js
+++ node/index.js
@@ -2,14 +2,46 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var redis = require('redis').createClient();
+
+const storeMessage = message => {
+  message = JSON.stringify(message);
+
+  if (message !== null) {
+    redis.lpush('messages', message, (err, response) => {
+      redis.ltrim('messages', 0, 9);
+    });
+  }
+};
 
 io.on('connection', client => {
   client.emit('user_connected');
-  console.log('Client connected...');
 
   client.on('join_chat', username => {
     client.username = username;
-    console.log(`${username} joined the chat!`);
+    client.broadcast.emit('new_client', `${username} joined the chat!`);
+
+    if (username !== null) {
+      redis.smembers('users', (err, users) => {
+        users.forEach(user => {
+          client.emit('new_client', `${user} is on the chat!`);
+        });
+      });
+
+      redis.sadd('users', username);
+
+      redis.lrange('messages', 0, -1, (err, messages) => {
+        messages = messages.reverse();
+
+        messages.forEach(msg => {
+          msg = JSON.parse(msg);
+
+          const message = `${msg.user}: ${msg.text}`;
+
+          client.emit('message_received', message);
+        });
+      });
+    }
   });
 
   client.on('send_message', text => {
@@ -17,7 +49,19 @@ io.on('connection', client => {
 
     client.broadcast.emit('message_received', message);
     client.emit('message_received', message);
-    console.log(`${client.username} said: ${text}.`);
+
+    storeMessage({ user: client.username, text });
+  });
+
+  client.on('disconnect', name => {
+    const username = client.username;
+    const message = `${username} left the chat!`;
+
+    client.broadcast.emit('client_left', message);
+
+    if (username !== null) {
+      redis.srem('users', username);
+    }
   });
 });
 

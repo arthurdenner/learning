39d224ae86c595a5472e6909a2d81181beb50815
arthurdenner <arthurdenner7@gmail.com>
2018-01-24 21:36:35 -0300

    (react-native): Creating TextInput for the Currency Converter app

---
+++ react-native/currency-converter/app/components/TextInput/InputWithButton.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, TextInput, TouchableHighlight, View } from 'react-native';
+import color from 'color';
+import styles from './styles';
+
+const InputWithButton = ({ buttonText, editable, onPress, ...rest }) => {
+  const underlayColor = color(styles.$buttonBackgroundColorBase).darken(
+    styles.$buttonBackgroundColorModifier
+  );
+
+  const containerStyles = [styles.container];
+  if (!editable) {
+    containerStyles.push(styles.containerDisabled);
+  }
+
+  return (
+    <View style={containerStyles}>
+      <TouchableHighlight
+        underlayColor={underlayColor}
+        onPress={onPress}
+        style={styles.buttonContainer}
+      >
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableHighlight>
+      <View style={styles.border} />
+      <TextInput
+        {...rest}
+        editable={editable}
+        style={styles.input}
+        underlineColorAndroid="transparent"
+      />
+    </View>
+  );
+};
+
+InputWithButton.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  editable: PropTypes.bool,
+  onPress: PropTypes.func,
+};
+
+InputWithButton.defaultProps = {
+  editable: true,
+  onPress: () => {},
+};
+
+export default InputWithButton;
--- react-native/currency-converter/app/screens/Home.js
+++ react-native/currency-converter/app/screens/Home.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
-import { Container, Logo } from '../components';
+import { Container, InputWithButton, Logo } from '../components';
 
-export default () => (
-  <Container>
-    <StatusBar barStyle="light-content" translucend={false} />
-    <Logo />
-  </Container>
-);
+class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base');
+  };
+
+  handlePressQuoteCurrency = () => {
+    console.log('press quote');
+  };
+
+  handleChangeText = text => {
+    console.log('change text', text);
+  };
+
+  render() {
+    return (
+      <Container>
+        <StatusBar barStyle="light-content" translucend={false} />
+        <Logo />
+        <InputWithButton
+          buttonText="USD"
+          defaultValue="100"
+          keyboardType="numeric"
+          onChangeText={this.handleChangeText}
+          onPress={this.handlePressBaseCurrency}
+        />
+        <InputWithButton
+          buttonText="GBP"
+          editable={false}
+          onPress={this.handlePressQuoteCurrency}
+          value="79.74"
+        />
+      </Container>
+    );
+  }
+}
+
+export default Home;

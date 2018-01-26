7d2370b3c13142027f09f9a20342f6c017081641
arthurdenner <arthurdenner7@gmail.com>
2018-01-25 21:23:13 -0300

    (react-native): Adding animation to the Logo when the keyboard pops up in the Currency Converter app

--- react-native/currency-converter/app/components/Logo/Logo.js
+++ react-native/currency-converter/app/components/Logo/Logo.js
@@ -1,22 +1,95 @@
-import React from 'react';
-import { Image, ImageBackground, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Animated,
+  Keyboard,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import styles from './styles';
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      resizeMode="contain"
-      source={require('./images/background.png')}
-      style={styles.containerImage}
-    >
-      <Image
-        resizeMode="contain"
-        source={require('./images/logo.png')}
-        style={styles.image}
-      />
-    </ImageBackground>
-    <Text style={styles.text}>Currency Converter</Text>
-  </View>
-);
+const isAndroid = Platform.OS === 'android';
+const ANIMATION_DURATION = 250;
+
+class Logo extends Component {
+  state = {
+    containerImageWidth: new Animated.Value(styles.$largeContainerSize),
+    imageWidth: new Animated.Value(styles.$largeImageSize),
+  };
+
+  componentDidMount() {
+    const showListener = isAndroid ? 'keyboardDidShow' : 'keyboardWillShow';
+    const hideListener = isAndroid ? 'keyboardDidHide' : 'keyboardWillHide';
+
+    this.keyboardShowListener = Keyboard.addListener(
+      showListener,
+      this.keyboardShow
+    );
+    this.keyboardHideListener = Keyboard.addListener(
+      hideListener,
+      this.keyboardHide
+    );
+  }
+
+  componentWillUnmount() {
+    this.keyboardShowListener.remove();
+    this.keyboardHideListener.remove();
+  }
+
+  keyboardShow = () => {
+    Animated.parallel([
+      Animated.timing(this.state.containerImageWidth, {
+        toValue: styles.$smallContainerSize,
+        duration: ANIMATION_DURATION,
+      }),
+      Animated.timing(this.state.imageWidth, {
+        toValue: styles.$smallImageSize,
+        duration: ANIMATION_DURATION,
+      }),
+    ]).start();
+  };
+
+  keyboardHide = () => {
+    Animated.parallel([
+      Animated.timing(this.state.containerImageWidth, {
+        toValue: styles.$largeContainerSize,
+        duration: ANIMATION_DURATION,
+      }),
+      Animated.timing(this.state.imageWidth, {
+        toValue: styles.$largeImageSize,
+        duration: ANIMATION_DURATION,
+      }),
+    ]).start();
+  };
+
+  render() {
+    const { containerImageWidth, imageWidth } = this.state;
+
+    const containerImageStyles = [
+      styles.containerImage,
+      { width: containerImageWidth, height: containerImageWidth },
+    ];
+    const imageStyles = [styles.logo, { width: imageWidth }];
+
+    return (
+      <View style={styles.container}>
+        <Animated.View style={containerImageStyles}>
+          <Animated.Image
+            resizeMode="contain"
+            style={[StyleSheet.absoluteFill, containerImageStyles]}
+            source={require('./images/background.png')}
+          />
+          <Animated.Image
+            resizeMode="contain"
+            source={require('./images/logo.png')}
+            style={imageStyles}
+          />
+        </Animated.View>
+        <Text style={styles.text}>Currency Converter</Text>
+      </View>
+    );
+  }
+}
 
 export default Logo;
--- react-native/currency-converter/app/components/Logo/styles.js
+++ react-native/currency-converter/app/components/Logo/styles.js
@@ -4,25 +4,30 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const imageWidth = Dimensions.get('window').width / 2;
 
 const styles = EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+
   container: {
-    alignItems: 'center'
+    alignItems: 'center',
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
   },
   image: {
-    width: imageWidth / 2
+    width: '$smallContainerSize',
   },
   text: {
     fontSize: 28,
     fontWeight: '600',
     letterSpacing: -0.5,
     marginTop: 15,
-    color: '$white'
-  }
+    color: '$white',
+  },
 });
 
 export default styles;
